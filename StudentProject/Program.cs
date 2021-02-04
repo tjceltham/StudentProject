7f2a3d6ed64d46ad0f405914d713ec3007311927
Tony Collins <tjc@eltham-college.org.uk>
2021-02-04 12:05:20 +0000

    Add project files.

---
+++ StudentProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StudentProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student("tjc", "Sydenham", "12\\34\\89");
+            s.sort();
+        }
+    }
+}
---
+++ StudentProject/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentProject
+{
+    class Student
+    {
+        private string name;
+        private string address;
+        private string dob;
+        private string[] subject;
+        private string[] grade;
+        private int countGrades = 0;
+        public Student(string n, string a, string d)
+        {
+            name = n;
+            address = a;
+            dob = d;
+            subject = new string[5];
+            grade = new string[5];
+        }
+
+
+        public bool addGrade(string sub, string grade)
+        {
+            bool added = false;
+            // check for duplicatted subject - update duplicatted.
+
+
+
+            countGrades++;
+            sort();
+            return added;
+        }
+
+        public void printStudentDetails()
+        {
+
+        }
+        private void sort()
+        {
+
+        }
+    }
+}
